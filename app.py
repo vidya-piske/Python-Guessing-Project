b19312ccc0d7594320dbdafef5b97488b3b1e416
vidya-piske <vidyapiske123@gmail.com>
2024-05-23 12:47:51 +0530

    made few modifications in the app.py and number_guessing_game.py

--- app.py
+++ app.py
@@ -1,21 +1,19 @@
-from flask import Flask, render_template
-from number_guessing_game import shuffle_list,get_user_guess,check_guess,mylist 
+from flask import Flask, render_template, request
+from number_guessing_game import shuffle_list, check_guess, mylist
 
 app = Flask(__name__)
 
-@app.route('/')
-
+@app.route('/', methods=['GET', 'POST'])
 def index():
-    # Shuffled List
-    shuffle_output = shuffle_list(mylist)
+    shuffle_output = shuffle_list(mylist)  
+    result = ""
+    user_guess_value = ""
 
-    # User Input
-    user_guess_value = get_user_guess()
-
-    # Guessing
-    result = check_guess(shuffle_output, user_guess_value)
-   
-    return render_template('index.html', result=result)
+    if request.method == 'POST':
+        user_guess_value = int(request.form['guess'])
+        result = check_guess(shuffle_output, user_guess_value)
+    
+    return render_template('index.html', result=result, shuffled_list=shuffle_output)
 
 if __name__ == "__main__":
-    app.run()
+    app.run(debug=True)
--- number_guessing_game.py
+++ number_guessing_game.py
@@ -4,18 +4,11 @@ def shuffle_list(mylist):
     shuffle(mylist)
     return mylist
 
-def get_user_guess():
-    guess = ''
-    while guess not in ['0', '1', '2']:
-        guess = input("Enter a value 0, 1, 2: ")
-    return int(guess)
-
 def check_guess(shuffled_list, user_guess):
-    if shuffled_list[user_guess] == '0':  
-        print(f"Correct value: {shuffled_list}")
+    if shuffled_list[user_guess] == '0':
+        return f"Correct! The list was: {shuffled_list}"
     else:
-        print(f"Wrong value: {shuffled_list}")
+        return f"Wrong guess. The list was: {shuffled_list}"
 
 # Initialization
 mylist = ['', '0', '']
-
