87017e97763e1f8484b5e418b7ef738f86d3fc3e
vidya-piske <vidyapiske123@gmail.com>
2024-05-23 11:49:18 +0530

    Added the logic for the game

---
+++ number_guessing_game.py
@@ -0,0 +1,29 @@
+from random import shuffle
+
+def shuffle_list(mylist):
+    shuffle(mylist)
+    return mylist
+
+def get_user_guess():
+    guess = ''
+    while guess not in ['0', '1', '2']:
+        guess = input("Enter a value 0, 1, 2: ")
+    return int(guess)
+
+def check_guess(shuffled_list, user_guess):
+    if shuffled_list[user_guess] == '0':  
+        print(f"Correct value: {shuffled_list}")
+    else:
+        print(f"Wrong value: {shuffled_list}")
+
+# Initialization
+mylist = ['', '0', '']
+
+# Shuffled List
+shuffle_output = shuffle_list(mylist)
+
+# User Input
+user_guess_value = get_user_guess()
+
+# Guessing
+check_guess(shuffle_output, user_guess_value)
